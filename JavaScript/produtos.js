a61093cda105e89245a0c821f85aabf00afff93f
elguesabal <113626409+elguesabal@users.noreply.github.com>
2022-10-13 16:00:09 -0300

    Add files via upload
    alteracoes feitas:
    -funcao gota retirada

--- JavaScript/produtos.js
+++ JavaScript/produtos.js
@@ -1,32 +1,4 @@
-function gota1 (foto)
-    {document.getElementById("a").src = foto;}
-
-function gota2 (foto)
-    {document.getElementById("b").src = foto;}
-
-function gota3 (foto)
-    {document.getElementById("c").src = foto;}
-
-function gota4 (foto)
-    {document.getElementById("d").src = foto;}
-
-function gota5 (foto)
-    {document.getElementById("e").src = foto;}
-
-function gota6 (foto)
-    {document.getElementById("f").src = foto;}
-
-function gota7 (foto)
-    {document.getElementById("g").src = foto;}
-
-    function gota8 (foto)
-    {document.getElementById("h").src = foto;}
-
-    function gota9 (foto)
-    {document.getElementById("i").src = foto;}
-
-
-    // funcção comprar
+   // funcção comprar
 
     function comprarProduto()
     {
